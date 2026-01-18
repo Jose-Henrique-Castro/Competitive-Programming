36915e99f37eac1db7bcdee8244efce7bed653b5
José Henrique Castro <josephenrycastro@gmail.com>
2026-01-18 13:50:40 -0300

    Add files via upload

--- Baekjoon-1260.cpp
+++ Baekjoon-1260.cpp
@@ -1,83 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector <bool> vis;
-vector <int> caminhoDFS;
-vector <int> caminhoBFS;
+vector<int> resultado_dfs;
+vector<int> resultado_bfs;
 
+void DFS (int u,vector <bool>& vis1,vector<vector<int>>&adj){
 
-vector <int> DFS(int end,int start,vector<vector<int>>& adj){
+    vis1[u] = true;
+    resultado_dfs.push_back(u);
 
-if(vis[start]) return caminhoDFS;
+    for(auto x : adj[u]){
 
-vis[start] = true;
-caminhoDFS.push_back(start);
+        if(!vis1[x]){
+            DFS(x,vis1,adj);
+        }
 
-for(auto x : adj[start]){
-
-    if(!vis[x]){
-        DFS(end,x,adj);
-    }
-}
-return caminhoDFS;
-}
-
-vector <int> BFS(int end,int start,vector<vector<int>>& adj){
-
-vis[start] = true;
-queue <int> q;
-q.push(start);
-caminhoBFS.push_back(start);
-
-while(!q.empty()){
-
-int atual = q.front();
-q.pop();
-
-for(auto vizinho : adj[atual]){
-
-    if(!vis[vizinho]){
-        vis[vizinho] = true;
-        caminhoBFS.push_back(vizinho);
-        q.push(vizinho);
     }
 
-}
 
 }
-return caminhoBFS;
-}
-
 
 int main(){
 
-int nodes,edges,start; cin >> nodes >> edges >> start;
+int v , c,start; cin >> v >> c >> start;
 
-vector <vector<int>> adj(nodes+1);
+vector<vector<int>> adj(v+1);
+vector<bool> vis1(v+1,false);
+vector<bool> vis2(v+1,false);
 
-for(int i=0;i<edges;i++){
-    int u,v; cin >> u >> v;
-    adj[u].push_back(v);
-    adj[v].push_back(u);
+for(int i=0;i<c;i++){
+int r,n; cin >> r >> n;
+adj[r].push_back(n);
+adj[n].push_back(r);
 }
 
-for(int i=1;i<=nodes;i++){
+for(int i=1;i<=v;i++){
     sort(adj[i].begin(),adj[i].end());
 }
 
-vis.assign(nodes+1,false);
-vector <int> DFS_ = DFS(nodes,start,adj);
-fill(vis.begin(),vis.end(),false);
-vector <int> BFS_ = BFS(nodes,start,adj);
+DFS(start,vis1,adj);
 
-for(int i=0;i<DFS_.size();i++){
-    cout << caminhoDFS[i] << (i == caminhoDFS.size() - 1 ? "" : " ");
+queue <int> fila;
+fila.push(start);
+vis2[start] = true;
+
+while(!fila.empty()){
+
+    int u = fila.front();
+    fila.pop();
+
+    resultado_bfs.push_back(u);
+
+    for(int x: adj[u]){
+
+        if(!vis2[x]){
+            vis2[x] = true;
+            fila.push(x);
+        }
+    }
+
+}
+
+for(int i=0;i<resultado_dfs.size();i++){
+
+cout << resultado_dfs[i];
+i < resultado_dfs.size()-1 ? cout << " " : cout << "";
+
 }
 cout << endl;
 
-for(int i=0;i<BFS_.size();i++){
-    cout << caminhoBFS[i] << (i == caminhoBFS.size() - 1 ? "" : " ");
-}
-cout << endl;
+for(int i = 0; i < resultado_bfs.size(); i++){
+        cout << resultado_bfs[i];
+        (i < resultado_bfs.size() - 1) ? cout << " " : cout << "";
+    }
+    cout << endl;
 
+    
     return 0;
 }
